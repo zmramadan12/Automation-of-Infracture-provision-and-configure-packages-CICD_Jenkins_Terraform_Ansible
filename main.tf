bcf8c65fb5da86273b9134f4524c9088f36ee904
Zahid Muhammad Ramadan <125232356+zmramadan12@users.noreply.github.com>
2025-08-22 23:28:46 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,24 @@
-## Defination of Cloud provider ##
 provider "aws" {
-  
-
-region = "ap-southeast-3"
+  region = "ap-southeast-3"
 }
 
-## Provision of EC2 server##
 resource "aws_instance" "ram" {
+  ami                    = "ami-0c82cd70874a842cf"
+  instance_type          = "t3.micro"
+  key_name               = "devops1"
+  subnet_id              = "subnet-049f77a69bee62037"
+  vpc_security_group_ids = ["sg-04049b72b0df7ca4d"]
 
-    instance_type = "t3.micro"
-    ami = "ami-0c82cd70874a842cf"
-    key_name = "devops1"
-    vpc_security_group_ids = ["sg-04049b72b0df7ca4d"]
-    tags = {
-        Name= "Developement Server"  
-    }
-#### After provision now will create inventory.ini ###
-     provisioner "local-exec" {
-        command = <<EOT
-       
-       echo "[webserver]">$WORKSPACE/inventory.ini
-       echo "${self.public_ip} ansible_user=ubuntu" >>$WORKSPACE/inventory.ini
-       sleep 60
-       echo "Terraform successfully run and Inventory created"        
-       EOT 
-     }
+  tags = {
+    Name = "Development Server"
+  }
 
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[webserver]">$WORKSPACE/inventory.ini
+      echo "${self.public_ip} ansible_user=ubuntu" >> $WORKSPACE/inventory.ini
+      sleep 60
+      echo "Terraform successfully run and Inventory created"
+    EOT
+  }
 }
