6846435a54cd7ca52614c2f9336a2a273815acd4
Zahid Muhammad Ramadan <125232356+zmramadan12@users.noreply.github.com>
2025-08-22 23:04:05 +0700

    Add files via upload

---
+++ JenkinsTerraformAnsiblesetup.sh
@@ -0,0 +1,43 @@
+##Jenkins installation# 
+
+echo ##Jenkins installation#
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+
+##Ansible Installtion#
+echo ##Ansible Installtion#
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible -y
+sudo apt install ansible -y
+
+
+##Terraform installtion#
+
+echo ##Terraform installtion# 
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update -y
+sudo apt-get install terraform -y
+
+
+echo "Jenkins & Ansible and Terraform Installed successfully."
---
+++ main.tf
@@ -0,0 +1,28 @@
+## Defination of Cloud provider ##
+provider "aws" {
+  
+
+region = "ap-southeast-3"
+}
+
+## Provision of EC2 server##
+resource "aws_instance" "ram" {
+
+    instance_type = "t3.micro"
+    ami = "ami-0f918f7e67a3323f0"
+    key_name = "devops1"
+    vpc_security_group_ids = ["sg-04049b72b0df7ca4d"]
+    tags = {
+        Name= "Developement Server"  
+    }
+#### After provision now will create inventory.ini ###
+     provisioner "local-exec" {
+        command = <<EOT
+       
+       echo "[webserver]">$WORKSPACE/inventory.ini
+       echo "${self.public_ip} ansible_user=ubuntu" >>$WORKSPACE/inventory.ini
+       sleep 60
+       echo "Terraform successfully run and Inventory created"        
+       EOT 
+     }
+}
